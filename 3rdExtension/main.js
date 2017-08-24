c8daf60fa610571cbf0244500a1cd9a03aaf0720
Alex Jaynes <dr.ocheyo@gmail.com>
2017-08-23 21:16:00 -0400

    fix milisenconds

--- 3rdExtension/main.js
+++ 3rdExtension/main.js
@@ -1,15 +1,37 @@
-let d = new Date();
-const morning = () => d.getHours() > 5 && d.getHours() < 10;
-const unwanted = ['https://www.codewars.com/users/g964', 'https://www.codewars.com/users/giacomosorbi'];
 
-if (morning()) {
+const isMorning = () => {
+  const d = new Date();
+  return (d.getHours() > 5 && d.getHours() < 10);
+};
 
-  let kata = document.querySelectorAll('.items-list');
-  let items = kata[0].querySelectorAll('.list-item');
+const onListPage = () => {
+	const here = document.location.pathname;
+	return here.test(/\/kata\/(latest|search)/); 
+};
+
+const milisecondsUntilRefresh = () => {
+  const d = new Date(); 
+  const secs = (10 - (d.getHours()) - 1) * 60 + (60 - d.getMinutes()) * 60 * 1000; 
+  if (!isMorning()) 
+  return secs;
+};
+
+const unwanted = ['https://www.codewars.com/users/g964', 
+				  'https://www.codewars.com/users/giacomosorbi'];
+
+const beforeBreakfast = () => {
+
+  if (!isMorning() && onListPage()) return null; 
+
+  const kata = document.querySelectorAll('.items-list');
+  const items = kata[0].querySelectorAll('.list-item');
   items.forEach (k => {
-    
-    let info = k.querySelector('.info-row');
-	let user = info.querySelectorAll('a', '.has-tip')[3];
+    const info = k.querySelector('.info-row');
+	const user = info.querySelectorAll('a', '.has-tip')[3];
 	if (user && (unwanted.includes(user.href))) k.remove();
   });
-}
+};
+
+beforeBreakfast();
+document.onscroll = beforeBreakfast;
+setTimeout(window.location.reload, milisecondsUntilRefresh());
